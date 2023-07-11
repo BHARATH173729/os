168b962dc2585d016ec1d3945076e4f02e6e4b52
BHARATH173729 <122071405+BHARATH173729@users.noreply.github.com>
2023-07-11 09:33:13 +0000

    commit

--- fsfc.c
+++ fsfc.c
@@ -1,39 +1,47 @@
 #include<stdio.h>
-int main()
-{
-    char pn[10][10];
+int main(){
+
     int arr[10],bur[10],star[10],finish[10],tat[10],wt[10],i,n;
     int totwt=0,tottat=0;
-    printf("enter the number of processes:");
+    printf("Enter the number of processes:");
     scanf("%d",&n);
-    for(i=0;i<n;i++)
-    {
-        printf("enter the Process name,Arrival Time&Burst Time:");
-        scanf("%s%d%d",pn[i],&arr[i],&bur[i]);
+    for(i=0;i<n;i++) {
+        printf("Enter the Arrival Time & Burst Time: ");
+        scanf("%d%d",&arr[i],&bur[i]);
     }
-    for(i=0;i<n;i++)
-    {
-        if(i==0)
-        {
+    for(i=0;i<n;i++) {
+        if(i==0) {
             star[i]=arr[i];
             wt[i]=star[i]-arr[i];
             finish[i]=star[i]+bur[i];
             tat[i]=finish[i]-arr[i];
-        }
-        else{
+        } else {
             star[i]=finish[i-1];
             wt[i]=star[i]-arr[i];
             finish[i]=star[i]+bur[i];
             tat[i]=finish[i]-arr[i];
         }
     }
-    printf("\nPName Arrtime Burstime Start TAT Finish");
-    for(i=0;i<n;i++)
-    {
-        printf("\n%s\t%6d\t\t%6d\t%6d\t%6d\t%6d",pn[i],arr[i],bur[i],star[i],tat[i],finish[i]);
+    printf("\nP Name\tArr time\tBurs time\tStart\tTAT\tFinish\n");
+    for(i=0;i<n;i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\t\t%d\t\t%d\n",i,arr[i],bur[i],star[i],tat[i],finish[i]);
         totwt+=wt[i];
         tottat+=tat[i];
     }
-    printf("\nAverage Waiting time:%f",(float)totwt/n);
-    printf("\n Average turn around time:%f",(float)tottat/n);
-    }
+    printf("\nAverage Waiting time: %f\n",(float)totwt/n);
+    printf("Average turn around time: %f\n",(float)tottat/n);
+}
+    /*
+    Enter the number of processes:3
+    Enter the Arrival Time & Burst Time: 2 3
+    Enter the Arrival Time & Burst Time: 5 6 
+    Enter the Arrival Time & Burst Time: 6 7
+
+    P Name  Arr time        Burs time       Start   TAT     Finish
+    P0      2               3               2               3               5
+    P1      5               6               5               6               11
+    P2      6               7               11              12              18
+
+    Average Waiting time:1.666667
+    Average turn around time:7.000000
+    */
--- sjf2.c
+++ sjf2.c
@@ -1,23 +1,17 @@
 #include<stdio.h>
-void printer(int processes[],int n,int burst_time[],int arrival_time[])
-{
+void printer(int processes[],int n,int burst_time[],int arrival_time[]) {
     int WT[n],TAT[n],total_WT=0,total_TAT=0,completion_time[n];
-    for (int i = 0; i < n; i++)//calculating times
-    {
-        if(i==0)
-        {
+    for (int i = 0; i < n; i++) {
+        if(i==0) {
             WT[0]=0;
             completion_time[0]=burst_time[0]+arrival_time[0];
             TAT[0]=completion_time[0]-arrival_time[0];
             total_TAT=TAT[0];
             continue;
         }
-        if(completion_time[i-1]<arrival_time[i])
-        {
+        if(completion_time[i-1]<arrival_time[i]) {
             completion_time[i]=arrival_time[i]+burst_time[i];
-        }
-        else
-        {
+        } else {
             completion_time[i]=completion_time[i-1]+burst_time[i];
         }
         TAT[i]=completion_time[i]-arrival_time[i];
@@ -26,24 +20,19 @@ void printer(int processes[],int n,int burst_time[],int arrival_time[])
         total_WT+=WT[i];
     }
     printf("Processes\tAT\tBT\tWT\tTAT\tCT\n");
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         printf("%d\t\t%d\t%d\t%d\t%d\t%d\n",processes[i],arrival_time[i],burst_time[i],WT[i],TAT[i],completion_time[i]);
     }
     printf("AVG waiting time: %2f\n",(float)total_WT/n);
     printf("AVG turnaround time: %2f\n",(float)total_TAT/n);
     printf("\n");
 }
-void sjf(int processes[],int n,int burst_time[],int arrival_time[])
-{
+void sjf(int processes[],int n,int burst_time[],int arrival_time[]) {
     int WT[n],TAT[n],total_WT=0,completion_time[n],total_TAT=0;
-    for (int i = 0; i < n; i++)
-    {
+    for (int i = 0; i < n; i++) {
         int shortest_job_index=i;
-        for (int j = i+1; j < n; j++)
-        {
-            if (burst_time[j]<burst_time[shortest_job_index])
-            {
+        for (int j = i+1; j < n; j++) {
+            if (burst_time[j]<burst_time[shortest_job_index]) {
                 shortest_job_index=j;
             }
         }
@@ -62,20 +51,32 @@ void sjf(int processes[],int n,int burst_time[],int arrival_time[])
     printf("SJF agorithm\n");
     printer(processes,n,burst_time,arrival_time);
 }
-void main()
-{
+void main() {
     int n;
     int quantum;
     printf("Enter the number of processes: ");
     scanf("%d",&n);
     int processes[n],burst_time[n],arrival_time[n];
-    for (int i = 0; i < n; i++)
-    {
-        printf("Enter the arrival time for process %d:",i+1);
-        scanf("%d",&arrival_time[i]);
-        printf("Enter the Burst time for processes %d:",i+1);
-        scanf("%d",&burst_time[i]);
+    for (int i = 0; i < n; i++) {
+        printf("Enter the arrival time and burst time for process %d:",i+1);
+        scanf("%d%d",&arrival_time[i], &burst_time[i]);
         processes[i]=i+1;
     }
     sjf(processes,n,burst_time,arrival_time);
 }
+    /*
+    Enter the number of processes: 3
+    Enter the arrival time for process 1:2
+    Enter the Burst time for processes 1:3
+    Enter the arrival time for process 2:5
+    Enter the Burst time for processes 2:6
+    Enter the arrival time for process 3:6
+    Enter the Burst time for processes 3:7
+    SJF agorithm
+    Processes       AT      BT      WT      TAT     CT
+    1               2       3       0       3       5
+    2               5       6       0       6       11
+    3               6       7       5       12      18
+    AVG waiting time: 1.666667
+    AVG turnaround time: 7.000000
+    */
